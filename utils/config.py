264538b013fec256af9f846751fe8c38cd14c8f8
Mehdi <mehdi.m894@gmail.com>
2026-02-16 09:28:55 +0000

    batch_size edited

--- utils/config.py
+++ utils/config.py
@@ -30,7 +30,7 @@ class BenchmarkConfig:
 
     iterations: int = 1000
     warmup_iterations: int = 100
-    batch_sizes: tuple = (1, 10, 100, 1000)
+    batch_sizes: tuple = (10, 100, 1000, 5000)
     test_scenarios: tuple = ("single_insert", "batch_insert", "concurrent_insert")
     result_output_dir: str = "./results"
 
